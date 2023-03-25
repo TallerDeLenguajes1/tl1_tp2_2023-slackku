a1ce5e9510bad135ee6f611d9205d316886178c7
slacku <bananuya3@gmail.com>
2023-03-25 11:22:13 -0300

    Punto 3 completado

---
+++ tp2_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+#define M 7
+void main()
+{
+    int i, j;
+    int mt[N][M], *pMt;
+
+    pMt = &mt[0][0];
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *pMt + i + j; 
+            *pMt = 1 + rand() % 100;
+            printf("%d ", (*pMt + i + j));
+        }
+        printf("\n");
+    }
+}
